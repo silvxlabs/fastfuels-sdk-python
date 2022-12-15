5a4fb383036550d6bff3380c7737bde92730c4b9
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2022-12-15 11:25:01 -0700

    Add two get treelist data functions for comparison (#2)

--- fastfuels_sdk/treelists.py
+++ fastfuels_sdk/treelists.py
@@ -11,6 +11,8 @@ from pathlib import Path
 from datetime import datetime
 
 # External imports
+import pandas as pd
+from pandas import DataFrame
 from requests.exceptions import HTTPError
 
 
@@ -18,8 +20,9 @@ class Treelist:
     """
     Treelist class for the FastFuels SDK.
     """
+
     def __init__(self, id: str, name: str, description: str, method: str,
-                 dataset_id: str,  status: str, created_on: str,
+                 dataset_id: str, status: str, created_on: str,
                  summary: dict, fuelgrids: list[str], version: str):
         """
         Initialize a Treelist object.
@@ -131,6 +134,66 @@ def list_treelists(dataset_id: str = None) -> list[Treelist]:
     return [Treelist(**treelist) for treelist in response.json()["treelists"]]
 
 
+def get_treelist_data(treelist_id: str, path: Path) -> Treelist:
+    """
+    Get a Treelist's data as a CSV file.
+
+    Parameters
+    ----------
+    treelist_id : str
+        The ID of the Treelist to retrieve data for.
+    path : Path
+        The path to save the CSV file to.
+
+    Returns
+    -------
+    Treelist
+        Treelist object associated with the passed ID at the current time.
+
+    Raises
+    ------
+    HTTPError
+        If the API returns an unsuccessful status code.
+    """
+    # Send the request to the API
+    endpoint_url = f"{API_URL}/treelists/{treelist_id}/data"
+    response = SESSION.get(endpoint_url)
+
+    # Raise an error if the API returns an unsuccessful status code
+    if response.status_code != 200:
+        raise HTTPError(response.json())
+
+    # Save the CSV file to the passed path
+    with open(path, "w") as file:
+        file.write(response.text)
+
+    return Treelist(**response.json())
+
+
+def get_treelist_dataframe(treelist_id: str) -> DataFrame:
+    """
+    Get a Treelist's data as a Pandas DataFrame.
+
+    Parameters
+    ----------
+    treelist_id
+
+    Returns
+    -------
+
+    """
+    # Send the request to the API
+    endpoint_url = f"{API_URL}/treelists/{treelist_id}/data"
+    response = SESSION.get(endpoint_url)
+
+    # Raise an error if the API returns an unsuccessful status code
+    if response.status_code != 200:
+        raise HTTPError(response.json())
+
+    # Save the CSV file to the passed path
+    return pd.read_csv(response.text)
+
+
 def update_treelist(treelist_id: str, name: str = None,
                     description: str = None) -> Treelist:
     """
@@ -178,6 +241,7 @@ def update_treelist(treelist_id: str, name: str = None,
         raise HTTPError(response.json())
 
     return Treelist(**response.json())
+
 
 def update_treelist_data(treelist_id: str) -> Treelist:
     """
