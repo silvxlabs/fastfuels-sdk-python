80269609b36801fb2eabec0209aaf0fa6d9ececa
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2022-12-27 10:43:15 -0700

    Add exports module (#4)

---
+++ fastfuels_sdk/exports.py
@@ -0,0 +1,89 @@
+"""
+FastFuels SDK Exports Module
+"""
+
+# Core imports
+from pathlib import Path
+
+# External imports
+import numpy as np
+import zarr.hierarchy
+from numpy import ndarray
+from scipy.io import FortranFile
+
+
+def write_zarr_file_to_quicfire(zroot: zarr.hierarchy.Group,
+                                output_dir: Path | str) -> None:
+    """
+    Write a FastFuels zarr file to a QUIC-Fire .dat input file stack.
+
+    Parameters
+    ----------
+    zroot
+    output_dir
+
+    Returns
+    -------
+
+    """
+    if isinstance(output_dir, str):
+        output_dir = Path(output_dir)
+    # if not output_dir.is_dir():
+    #     raise ValueError("Output directory must be a directory.")
+
+    try:
+        canopy_group = zroot["canopy"]
+        surface_group = zroot["surface"]
+    except KeyError:
+        raise ValueError("The zarr file does not contain the required "
+                         "'surface' and 'canopy' groups.")
+
+    # Write bulk-density data to a .dat file
+    bulk_density_array = canopy_group["bulk-density"][...]
+    bulk_density_array[..., 0] += surface_group["bulk-density"][...]
+    write_np_array_to_dat(bulk_density_array, "treesrhof.dat", output_dir)
+    del bulk_density_array
+
+    # Write Fuel Moisture Content (FMC) data to a .dat file
+    fmc_array = canopy_group["FMC"][...]
+    fmc_array[..., 0] = surface_group["FMC"][...]
+    write_np_array_to_dat(fmc_array, "treesmoist.dat", output_dir)
+    del fmc_array
+
+    # Write fuel depth data to a .dat file
+    fuel_depth_array = np.zeros_like(canopy_group["bulk-density"][...])
+    fuel_depth_array[..., 0] = surface_group["fuel-depth"][...]
+    write_np_array_to_dat(fuel_depth_array, "treesfueldepth.dat",
+                          output_dir)
+    del fuel_depth_array
+
+    # Write DEM data to a .dat file
+    dem_array = surface_group["DEM"][...]
+    write_np_array_to_dat(dem_array, "topo.dat", output_dir)
+
+
+def write_np_array_to_dat(array: ndarray, dat_name: str,
+                          output_dir: Path) -> None:
+    """
+    Write a numpy array to a 32-bit float fortran binary file.
+
+    Parameters
+    ----------
+    array
+    dat_name
+    output_dir
+
+    Returns
+    -------
+
+    """
+    # Cast the data to 32-bit Float for Fortran
+    if len(array.shape) == 2:
+        data = np.array(array, dtype=np.float32)
+    else:
+        # Reshape array from (y, x, z) to (z, y, x) (also for fortran)
+        data = np.moveaxis(array, 2, 0).astype(np.float32)
+
+    # Write the zarr array to a dat file with scipy FortranFile package
+    f = FortranFile(Path(output_dir, dat_name), "w", "uint32")
+    f.write_record(data)
---
+++ tests/test_exports.py
@@ -0,0 +1,70 @@
+"""
+Test Exports module
+"""
+
+# Internal imports
+from fastfuels_sdk.exports import *
+
+
+def test_write_zarr_file_to_quicfire():
+    """
+    Test writing a FastFuels zarr file to a QUIC-Fire .dat input file stack.
+    """
+    # Load the test zarr file
+    test_zroot = zarr.open("test-data/test_fuelgrid.zip")
+
+    # Get the domain size from the zarr file
+    nx = test_zroot.attrs["nx"]
+    ny = test_zroot.attrs["ny"]
+    nz = test_zroot.attrs["nz"]
+
+    # Add an FMC array to the canopy group. All values should be 1.5 where the
+    # canopy is present and 0.0 where the canopy is absent.
+    canopy_group = test_zroot["canopy"]
+    canopy_group["FMC"][...] = np.where(canopy_group["bulk-density"][...] > 0.0, 1.5, 0.0)
+
+    # Write the test zarr file to a QUIC-Fire .dat input file stack
+    write_zarr_file_to_quicfire(test_zroot, "test-data")
+
+    # Combine the surface and canopy bulk density arrays into a single array
+    bd_array = canopy_group["bulk-density"][...]
+    bd_array[..., 0] += test_zroot["surface"]["bulk-density"][...]
+
+    # Load the treesrhof.dat file and check that the values are the
+    # same as the bulk density array
+    treesrhof_dat = FortranFile("test-data/treesrhof.dat", "r")
+    treesrhof_array = treesrhof_dat.read_reals(dtype=np.float32)
+    treesrhof_array = treesrhof_array.reshape((nz, ny, nx))
+    treesrhof_array = np.moveaxis(treesrhof_array, 0, 2).astype(np.float32)
+    assert np.allclose(treesrhof_array, bd_array)
+
+    # Combine the surface and canopy FMC arrays into a single array
+    fmc_array = canopy_group["FMC"][...]
+    fmc_array[..., 0] = test_zroot["surface"]["FMC"][...]
+
+    # Load the treesmoist.dat file and check that the values are the
+    # same as the FMC array
+    treesmoist_dat = FortranFile("test-data/treesmoist.dat", "r")
+    treesmoist_array = treesmoist_dat.read_reals(dtype=np.float32)
+    treesmoist_array = treesmoist_array.reshape((nz, ny, nx))
+    treesmoist_array = np.moveaxis(treesmoist_array, 0, 2).astype(np.float32)
+    assert np.allclose(treesmoist_array, fmc_array)
+
+    # Combine the surface and canopy fuel depth arrays into a single array
+    fd_array = np.zeros_like(canopy_group["bulk-density"][...])
+    fd_array[..., 0] = test_zroot["surface"]["fuel-depth"][...]
+
+    # Load the treesfueldepth.dat file and check that the values are the
+    # same as the fuel-depth array
+    treesfueldepth_dat = FortranFile("test-data/treesfueldepth.dat", "r")
+    treesfueldepth_array = treesfueldepth_dat.read_reals(dtype=np.float32)
+    treesfueldepth_array = treesfueldepth_array.reshape((nz, ny, nx))
+    treesfueldepth_array = np.moveaxis(treesfueldepth_array, 0, 2).astype(np.float32)
+    assert np.allclose(treesfueldepth_array, fd_array)
+
+    # Load the topo.dat file and check that the values are the
+    # same as the DEM array
+    topo_dat = FortranFile("test-data/topo.dat", "r")
+    topo_array = topo_dat.read_reals(dtype=np.float32)
+    topo_array = topo_array.reshape((ny, nx))
+    assert np.allclose(topo_array, test_zroot["surface"]["DEM"][...])
