81e7581f4d9a3fc6fae91b2daf81bb1c78c120a2
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2023-01-20 16:15:27 -0700

    Add module imports (#6)

--- fastfuels_sdk/__init__.py
+++ fastfuels_sdk/__init__.py
@@ -1,23 +1,14 @@
-import os
-import requests
+from fastfuels_sdk.datasets import Dataset, create_dataset
+from fastfuels_sdk.treelists import Treelist, create_treelist
+from fastfuels_sdk.fuelgrids import Fuelgrid, create_fuelgrid
+from fastfuels_sdk.exports import write_zarr_to_quicfire
 
-# Load the API key from the environment
-API_KEY = os.getenv("X_API_KEY")
-
-# Check if the API key is valid
-if API_KEY is None:
-    raise ValueError("The Application Default Credentials are not available. "
-                     "The environment variable X_API_KEY must be defined "
-                     "containing a valid API key.")
-
-# Use the key to access the API
-HEADERS = {"X-API-KEY": API_KEY}
-
-# Create a requests module session
-SESSION = requests.Session()
-SESSION.headers.update(HEADERS)
-
-# Define the live API URL
-API_URL = "https://fastfuels.silvx.io"
-
-
+__all__ = [
+    "Dataset",
+    "create_dataset",
+    "Treelist",
+    "create_treelist",
+    "Fuelgrid",
+    "create_fuelgrid",
+    "write_zarr_to_quicfire",
+]
