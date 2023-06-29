7a4c7e38a11c00e47ec869f20bbc50029162965e
amarcozzi <anthonymarcozzi@gmail.com>
2023-06-29 10:08:36 -0600

    Update API module (#7)

--- fastfuels_sdk/api.py
+++ fastfuels_sdk/api.py
@@ -1,60 +1,22 @@
 import os
 import requests
 
+# Load the API key from the environment
+api_key = os.getenv("FASTFUELS_API_KEY")
 
-class FastFuelsAPI:
-    """
-    FastFuelsAPI class for the FastFuels SDK.
+# Check if the API key is valid
+if api_key is None:
+    raise ValueError(
+        "The Application Default Credentials are not available. "
+        "The environment variable FASTFUELS_API_KEY must be defined "
+        "containing a valid API key.")
 
-    This class provides an interface for accessing the FastFuels API. It handles
-    authentication and session management, allowing users to make requests to
-    the FastFuels API and retrieve data.
+# Use the key to access the API
+headers = {"X-API-KEY": api_key}
 
-    Attributes
-    ----------
-    headers : dict
-        A dictionary containing the headers for API requests, including the
-        API key.
-    session : requests.Session
-        A session object for making HTTP requests to the FastFuels API.
-    api_url : str
-        The URL of the FastFuels API.
+# Create a requests module session
+SESSION = requests.Session()
+SESSION.headers.update(headers)
 
-    Methods
-    -------
-    None
-
-    """
-
-    def __init__(self):
-        """
-        Initialize a FastFuelsAPI object.
-
-        The FastFuelsAPI object is responsible for handling authentication and
-        session management for requests to the FastFuels API.
-
-        Raises
-        ------
-        ValueError
-            If the API key is not available in the environment variables.
-
-        """
-        # Load the API key from the environment
-        api_key = os.getenv("FASTFUELS_API_KEY")
-
-        # Check if the API key is valid
-        if api_key is None:
-            raise ValueError(
-                "The Application Default Credentials are not available. "
-                "The environment variable FASTFUELS_API_KEY must be defined "
-                "containing a valid API key.")
-
-        # Use the key to access the API
-        self.headers = {"X-API-KEY": api_key}
-
-        # Create a requests module session
-        self.session = requests.Session()
-        self.session.headers.update(self.headers)
-
-        # Define the live API URL
-        self.api_url = "https://fastfuels.silvx.io"
+# Define the live API URL
+API_URL = "https://fastfuels.silvx.io"
