70630bfd5550679c1644ad5a19722590e7d21260
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2024-07-16 14:00:09 -0600

    Add client library generation scripts

---
+++ fastfuels_sdk/client_library_test/generate.sh
@@ -0,0 +1,4 @@
+# curl https://api-prod-nyvjyh5ywa-uw.a.run.app/openapi.json > api_spec.json
+curl http://127.0.0.1:8080/openapi.json > api_spec.json
+
+openapi-generator generate -i api_spec.json -g python -o ./python_client
