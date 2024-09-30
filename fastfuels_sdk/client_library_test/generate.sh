6897cdc5a5301bca2a9c158d4e82c2699ee514fb
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2024-09-30 12:14:32 -0600

    Remove pre commit caching

--- fastfuels_sdk/client_library_test/generate.sh
+++ fastfuels_sdk/client_library_test/generate.sh
@@ -1,4 +1,4 @@
-# curl https://api-prod-nyvjyh5ywa-uw.a.run.app/openapi.json > api_spec.json
+# curl https://api.fastfuels.silvxlabs.com/openapi.json > api_spec.json
 curl http://127.0.0.1:8080/openapi.json > api_spec.json
 
 openapi-generator generate -i api_spec.json -g python -o ./python_client
