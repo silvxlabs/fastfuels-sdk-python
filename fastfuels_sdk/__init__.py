272ca3b465ea1c8fe0972922d9aff7604168ebe1
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2024-12-31 16:15:36 -0700

    Add Features objects to package namespace

--- fastfuels_sdk/__init__.py
+++ fastfuels_sdk/__init__.py
@@ -1,5 +1,6 @@
 from fastfuels_sdk.domains import Domain, list_domains
 from fastfuels_sdk.inventories import Inventories, TreeInventory
+from fastfuels_sdk.features import Features, RoadFeature, WaterFeature
 from fastfuels_sdk.grids import Grids, SurfaceGrid, SurfaceGridBuilder, TreeGrid
 
 
@@ -8,6 +9,9 @@ __all__ = [
     "list_domains",
     "Inventories",
     "TreeInventory",
+    "Features",
+    "RoadFeature",
+    "WaterFeature",
     "Grids",
     "SurfaceGrid",
     "SurfaceGridBuilder",
